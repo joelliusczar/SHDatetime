3c6ffe77cc1e3ab516e4041b39bc32f0b587bfad
Joel Gillette <joelliuscaesar@gmail.com>
2018-10-15 23:06:01 -0400

    moved some stuff around, deleted unused constants.

--- SH_CTools/SHConstants.c
+++ SH_CTools/SHConstants.c
@@ -1,12 +1,11 @@
 //
-//  c_SHConstants.c
-//  SHCommon
+//  SHConstants.c
+//  SH_CTools
 //
-//  Created by Joel Pridgen on 4/1/18.
+//  Created by Joel Pridgen on 10/15/18.
 //  Copyright © 2018 Joel Gillette. All rights reserved.
 //
 
 #include "SHConstants.h"
 const int NOT_FOUND = -1;
 const int SKIP = -2;
-const int DAYS_IN_WEEK = 7;
